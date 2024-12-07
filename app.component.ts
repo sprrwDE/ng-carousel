e842ad414e6440afd29e31be9915cae393f08285
Kai Schulz <info@sparrowmedia.de>
2024-12-07 16:37:49 +0100

    working

--- app.component.ts
+++ app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwiperComponent } from './slider/swiper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwiperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
